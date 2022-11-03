205da84e7adef67484d37f32260ef63490759eb0
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-03 17:55:59 +0100

    Primer textfield

--- lib/pages/keyboard_types_page.dart
+++ lib/pages/keyboard_types_page.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 
 class KeyboardTypesPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: ListView(
+        padding: EdgeInsets.all(8),
         children: [
-
+          TextField(
+            // Para definir el tipo de teclado que queremos mostrar (TextInputType.emailAddress)
+            keyboardType: TextInputType.emailAddress,
+            // Para cambiar el aspecto del Textfield
+            decoration: InputDecoration(
+              // Para poner un titulo al Textfield
+              label: Text("Email"),
+              border: OutlineInputBorder(),
+            ),
+          ),
         ],
       ),
     );
   }
-
 }
