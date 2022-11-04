9413dc872888af7a1e4f314638686b410a3638bb
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-04 17:51:38 +0100

    Checkbox

--- lib/pages/check_box_page.dart
+++ lib/pages/check_box_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class CheckBoxPage extends StatefulWidget {
-
   @override
   State<CheckBoxPage> createState() => _CheckBoxPageState();
 }
@@ -15,6 +14,31 @@ class _CheckBoxPageState extends State<CheckBoxPage> {
         child: SizedBox(
           width: double.infinity,
           height: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Text(
+                  '''
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                 dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                  It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+                  It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with 
+                  desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                ''',
+                ),
+                Checkbox(
+                  // Valor que se debe pasar en caso de que no tengamos un valor predefinido en value
+                  tristate: true,
+                    // Valor que representa el checkbox
+                    value: null,
+                    // Funcion que se ha de pasar con el valor del booleano que llega por callback
+                    onChanged: (isChanged) {
+                      print('🧨');
+                    })
+              ],
+            ),
+          ),
         ),
       ),
     );
