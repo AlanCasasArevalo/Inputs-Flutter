f4e64934809977461cc17f6590f4ead00040ad1c
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-04 22:23:26 +0100

    Se configura el slider

--- lib/pages/slider_page.dart
+++ lib/pages/slider_page.dart
@@ -1,13 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:inputs/app_routes.dart';
 
-class SliderPage extends StatelessWidget {
+class SliderPage extends StatefulWidget {
+  @override
+  State<SliderPage> createState() => _SliderPageState();
+}
+
+class _SliderPageState extends State<SliderPage> {
+  double _value = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            children: [
+              Slider(
+                // Hacemos que se muestre un texto arriba cuando arrastramos el slider
+                label: '$_value',
+                // Es el valor (double) que debemos pasarle al Slider
+                value: _value,
+                // Valores minimo que va a aceptar el slider (dividira el valor minimo hasta llegar al maximo)
+                // OJO el valor minimo no puede ser inferior a nuestro _value
+                min: 3,
+                // Valor maximo que alcanzara el slider
+                // OJO el valor mayor no puede ser mayor a nuestro _value
+                max: 4,
+                // Divisiones que tendra el slider
+                divisions: 10,
+                onChanged: (valueChanged) {
+                  setState(() {
+                    // la funcion floorToDouble lo que hace es redondear el valor siempre para no tener decimales
+                    _value = valueChanged.floorToDouble();
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
-
 }
