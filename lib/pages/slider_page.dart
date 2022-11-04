35cb158f5d1225f88b3effe70d729a02f99d540d
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-04 22:27:40 +0100

    Se envuelve el Slider en un RotatedBox para poder girar el slider

--- lib/pages/slider_page.dart
+++ lib/pages/slider_page.dart
@@ -19,25 +19,34 @@ class _SliderPageState extends State<SliderPage> {
           height: double.infinity,
           child: Column(
             children: [
-              Slider(
-                // Hacemos que se muestre un texto arriba cuando arrastramos el slider
-                label: '$_value',
-                // Es el valor (double) que debemos pasarle al Slider
-                value: _value,
-                // Valores minimo que va a aceptar el slider (dividira el valor minimo hasta llegar al maximo)
-                // OJO el valor minimo no puede ser inferior a nuestro _value
-                min: 3,
-                // Valor maximo que alcanzara el slider
-                // OJO el valor mayor no puede ser mayor a nuestro _value
-                max: 4,
-                // Divisiones que tendra el slider
-                divisions: 10,
-                onChanged: (valueChanged) {
-                  setState(() {
-                    // la funcion floorToDouble lo que hace es redondear el valor siempre para no tener decimales
-                    _value = valueChanged.floorToDouble();
-                  });
-                },
+              Row(
+                children: [
+                  RotatedBox(
+                    // Parametro que permite rotar el slider valores de 1-4 (de ahi lo de quarter)
+                    quarterTurns: 1,
+                    child: Slider(
+                      // Hacemos que se muestre un texto arriba cuando arrastramos el slider
+                      label: '$_value',
+                      // Es el valor (double) que debemos pasarle al Slider
+                      value: _value,
+                      // Valores minimo que va a aceptar el slider (dividira el valor minimo hasta llegar al maximo)
+                      // OJO el valor minimo no puede ser inferior a nuestro _value
+                      min: 0,
+                      // Valor maximo que alcanzara el slider
+                      // OJO el valor mayor no puede ser mayor a nuestro _value
+                      max: 4,
+                      // Divisiones que tendra el slider
+                      divisions: 10,
+                      onChanged: (valueChanged) {
+                        setState(() {
+                          // la funcion floorToDouble lo que hace es redondear el valor siempre para no tener decimales
+                          _value = valueChanged.floorToDouble();
+                        });
+                      },
+                    ),
+                  ),
+                  Text("Hola que hase")
+                ],
               ),
             ],
           ),
