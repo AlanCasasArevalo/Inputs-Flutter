8439b6ac826c52cda54cdb3af809203bb252437b
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-04 21:45:23 +0100

    Se crean 2 funciones para hacer el codigo mas limpio Se cambia el Radio por un RadioListTile

--- lib/pages/radio_button_page.dart
+++ lib/pages/radio_button_page.dart
@@ -9,6 +9,18 @@ class _RadioButtonPageState extends State<RadioButtonPage> {
   String? _better;
   String? _gender;
 
+  void _onBetterChanged(String? valueReceived) {
+    setState(() {
+      _better = valueReceived;
+    });
+  }
+
+  void _onGenderChanged(String? valueReceived) {
+    setState(() {
+      _gender = valueReceived;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,79 +30,40 @@ class _RadioButtonPageState extends State<RadioButtonPage> {
           padding: EdgeInsets.all(16),
           children: [
             Text('Which is better?'),
-            // Se debe poner el tipo que vamos a manejar debido a que Radio trabaja con tipos genericos,
-            Row(
-              children: [
-                Radio<String>(
-                  value: 'Flutter',
-                  groupValue: _better,
-                  onChanged: (textChanged) {
-                    setState(() {
-                      _better = textChanged;
-                    });
-                  },
-                ),
-                Text(
-                  'Flutter',
-                ),
-              ],
+            // El RadioListTile es similar al Radio, pero permite que sea directamente una celda completa
+            RadioListTile<String>(
+              value: 'Flutter',
+              title: Text(
+                'Flutter',
+              ),
+              groupValue: _better,
+              onChanged: _onBetterChanged,
             ),
-            Row(
-              children: [
-                Radio<String>(
-                  // Debemos poner el valor del radio button
-                  value: 'React-native',
-                  // El valor del grupo
-                  groupValue: _better,
-                  // El cambio de valor
-                  onChanged: (textChanged) {
-                    setState(() {
-                      _better = textChanged;
-                    });
-                  },
-                ),
-                Text(
-                  'React-native',
-                ),
-              ],
+            RadioListTile<String>(
+              value: 'React-native',
+              title: Text(
+                'React-native',
+              ),
+              groupValue: _better,
+              onChanged: _onBetterChanged,
             ),
             Divider(),
             Text('What is your gender?'),
-            // Si pasasemos la misma variable a los 4 _groupValue, serian de la misma pregunta
-            Row(
-              children: [
-                Radio<String>(
-                  value: 'Male',
-                  groupValue: _gender,
-                  onChanged: (textChanged) {
-                    setState(() {
-                      _gender = textChanged;
-                    });
-                  },
-                ),
-                Text(
-                  'Male',
-                ),
-              ],
+            RadioListTile<String>(
+              title: Text(
+                'Male',
+              ),
+              value: 'Male',
+              groupValue: _gender,
+              onChanged: _onGenderChanged,
             ),
-            Row(
-              children: [
-                Radio<String>(
-                  // Debemos poner el valor del radio button
-                  value: 'Female',
-                  // El valor del grupo
-                  groupValue: _gender,
-                  // El cambio de valor
-                  onChanged: (textChanged) {
-                    setState(() {
-                      _gender = textChanged;
-                    });
-                  },
-                ),
-                Text(
-                  'Female',
-                ),
-              ],
+            RadioListTile<String>(
+              title: Text(
+                'Female',
+              ),
+              value: 'Female',
+              groupValue: _gender,
+              onChanged: _onGenderChanged,
             ),
           ],
         ),
