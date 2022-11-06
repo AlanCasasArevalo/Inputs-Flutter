4970981882ff5e93e166ce98175b26d969306f3e
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-06 06:48:19 +0100

    Funciones basicas de calendar picker

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -6,15 +6,22 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: SafeArea(
-        child: SizedBox(
-          width: double.infinity,
-          height: double.infinity,
+        child: CalendarDatePicker(
+          // El año en la que se posiciona el calendario (puede o no coincidir con la fecha en la que se comienza el datepicker
+          initialDate: DateTime(2000),
+          // El año con la que se comienza el calendario OJO SI SELECCIONAS UNA FECHA Y LUEGO CAMBIAS EL CALENDARIO PARA QUE EMPIECE EN UNA FECHA POSTERIOR, TENDRAS QUE HACER UN HOT RELOAD IGUAL EN LA FECHA DE FIN
+          firstDate: DateTime(1900),
+          // El año con la que se termina el calendario
+          lastDate: DateTime.now(),
+          // La funcion nos devuelve la fecha cuando seleccionamos una
+          onDateChanged: (date) {
+            print(date);
+          },
         ),
       ),
     );
