b12df7198da6abb50773d16b5cdf215853033634
Alan Casas <alan_casas_arevalo@carrefour.com>
2022-11-06 06:56:51 +0100

    Funciones de cambio de fecha seleccionada

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -6,21 +6,42 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
+  late DateTime _date;
+  late DateTime _initialDate;
+
+  @override
+  void initState() {
+    super.initState();
+    _initialDate = DateTime(1980, 12, 11);
+    _date = _initialDate;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: _date == _initialDate ? null : () {
+            },
+            icon: Icon(Icons.save),
+          ),
+        ],
+      ),
       body: SafeArea(
         child: CalendarDatePicker(
           // El año en la que se posiciona el calendario (puede o no coincidir con la fecha en la que se comienza el datepicker
-          initialDate: DateTime(2000),
+          initialDate: _date,
           // El año con la que se comienza el calendario OJO SI SELECCIONAS UNA FECHA Y LUEGO CAMBIAS EL CALENDARIO PARA QUE EMPIECE EN UNA FECHA POSTERIOR, TENDRAS QUE HACER UN HOT RELOAD IGUAL EN LA FECHA DE FIN
           firstDate: DateTime(1900),
           // El año con la que se termina el calendario
           lastDate: DateTime.now(),
           // La funcion nos devuelve la fecha cuando seleccionamos una
           onDateChanged: (date) {
-            print(date);
+            setState(() {
+              _date = date;
+              print(date);
+            });
           },
         ),
       ),
